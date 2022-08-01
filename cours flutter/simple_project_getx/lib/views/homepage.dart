62456c9f8e9d09e4647b35c6d0bde321243bf30d
Elie Kattour <ekattour1@gmail.com>
2022-08-01 11:40:44 +0300

    changes

--- cours flutter/simple_project_getx/lib/views/homepage.dart
+++ cours flutter/simple_project_getx/lib/views/homepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -12,83 +14,122 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: GetX<CartController>(builder: (controller) {
-            return Text(controller.count.toString());
-          }),
-        ),
-        backgroundColor: Colors.amberAccent,
-        body: Column(
-          children: [
-            Expanded(
-              child: GetX<ShoppingController>(builder: (controller) {
-                return ListView.builder(
-                  itemCount: controller.products.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  // CircleAvatar(
-                                  //   child: Image(image: NetworkImage('${controller.products[index].productImage}')),
-                                  // ),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        '${controller.products[index].productName}',
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Text(
-                                        '${controller.products[index].productDescription}',
-                                        style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.normal),
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    '\$${controller.products[index].price}',
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                cartController
-                                    .addToCart(controller.products[index]);
-                              },
-                              child: Text("Add to Cart"),
-                            ),
-                          ],
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Text(
+            "Home",
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Stack(
+                    children: [
+                      Positioned(
+                        bottom: 24,
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(184, 245, 186, 182),
+                            shape: BoxShape.circle,
+                          ),
+                          child: GetX<CartController>(builder: (controller) {
+                            return Text(
+                              controller.count.toString(),
+                              style: TextStyle(color: Colors.black),
+                            );
+                          }),
                         ),
                       ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.add_shopping_cart,
+                            size: 26,
+                          )),
+                    ],
+                  ),
+                  GetX<CartController>(builder: (controller) {
+                    return Text(
+                      "\$ ${controller.totalPrice} ",
+                      style: TextStyle(fontSize: 18),
                     );
-                  },
-                );
-              }),
-            ),
-            GetX<CartController>(builder: (controller) {
-              return Text(
-                "Total amout: \$ ${controller.totalPrice} ",
-                style: TextStyle(fontSize: 32),
-              );
-            }),
-            SizedBox(height: 100),
+                  }),
+                ],
+              ),
+            )
           ],
+        ),
+        backgroundColor: Colors.amberAccent,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: GetX<ShoppingController>(builder: (controller) {
+                  return ListView.builder(
+                    itemCount: controller.products.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    // CircleAvatar(
+                                    //   child: Image(image: NetworkImage('${controller.products[index].productImage}')),
+                                    // ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          '${controller.products[index].productName}',
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          '${controller.products[index].productDescription}',
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.normal),
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      '\$${controller.products[index].price}',
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  cartController
+                                      .addToCart(controller.products[index]);
+                                },
+                                child: Text("Add to Cart"),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
